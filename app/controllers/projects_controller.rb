5b0a736499b4e0ce947f61b3e415855e4b59a1c5
Agathe Lurquin <agathe.lurquin@essca.eu>
2022-06-08 14:49:57 -0300

    Project routes new, create, show + actions in controller + show view

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,22 @@
 class ProjectsController < ApplicationController
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+    # condition if
+    @project.save
+    # redirect to project_path(@project), the view page of the created project
+  end
+
+  def show
+    @project = Project.find(params[:id])
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:name, :description, :status, :price, :due_date)
+  end
 end
