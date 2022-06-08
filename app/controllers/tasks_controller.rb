ba696daa9df8de73ce6e8571982e3d3e2f817d91
54rtor1 <1929sartori@gmail.com>
2022-06-08 14:59:41 -0300

    task form and def show for testing

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,8 @@
 class TasksController < ApplicationController
+  def show
+  @task = Task.find(params[:id])
+  end
+
   def new
     @task = Task.new
   end
@@ -20,6 +24,7 @@ class TasksController < ApplicationController
   end
 
   def update
+    @task = Task.find(params[:id])
     @task.update(task_params)
     redirect_to task_path(@task)
   end
