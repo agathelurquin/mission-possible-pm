7023fc3a3e50820043f464ac3b088b8f39ca7f3f
54rtor1 <1929sartori@gmail.com>
2022-06-08 12:06:38 -0300

    task new and create controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,26 @@
 class TasksController < ApplicationController
+  def new
+    @task = Task.new
+  end
 
+  def create
+    @task = Task.find(params[:id])
+    if @task.save
+      redirect_to task_path(@task)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 
+  def edit
+  end
+
+  def update
+  end
+
+  private
+
+  def dining_params
+    params.require(:task).permit(:name, :description, :price, :due_date)
+  end
 end
