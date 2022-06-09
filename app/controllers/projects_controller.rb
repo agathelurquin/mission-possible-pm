981745215ea27c08d6006f072a7efca449f15413
Adriana Osorio <driosorio@gmail.com>
2022-06-09 10:09:46 -0300

    Julia fixed project create

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -7,6 +7,7 @@ class ProjectsController < ApplicationController
 
   def create
     @project = Project.new(project_params)
+    @project.user = current_user
     if @project.save
       redirect_to project_path(@project)
     else
