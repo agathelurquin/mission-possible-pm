7a7cca907a7bde80b14a2f6bb7650b22ec6cf629
54rtor1 <1929sartori@gmail.com>
2022-06-13 11:40:38 -0300

    chatroom name method

--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -1,3 +1,12 @@
 class Chatroom < ApplicationRecord
+  before_validation :chatroom_name
+
   has_many :messages
+  belongs_to :project
+
+  private
+
+  def chatroom_name
+    self.name = self.project.name
+  end
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -2,6 +2,7 @@ class Project < ApplicationRecord
   belongs_to :user
   has_many :tasks, dependent: :destroy
   has_many :assignments, through: :tasks
+  has_one :chatroom, dependent: :destroy
 
   validates :name, :description, :due_date, presence: true
 
