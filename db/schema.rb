b1bd28c56e12d810d69299207c0fd6fe4cbb7108
lucasmendesdias <lucasbiocelp@gmail.com>
2022-06-13 13:39:47 -0300

    create migration of project

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_08_144308) do
+ActiveRecord::Schema.define(version: 2022_06_13_163719) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,7 +27,7 @@ ActiveRecord::Schema.define(version: 2022_06_08_144308) do
   create_table "projects", force: :cascade do |t|
     t.string "name"
     t.string "description"
-    t.string "status"
+    t.string "status", default: "Open"
     t.float "price"
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
