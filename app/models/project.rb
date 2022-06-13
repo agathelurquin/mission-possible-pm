7e8ddabeb58d693b665a3b614bef9e40e0722240
Agathe Lurquin <agathe.lurquin@essca.eu>
2022-06-13 14:56:10 -0300

    aws s3 working for media

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,7 +2,7 @@ class Project < ApplicationRecord
   belongs_to :user
   has_many :tasks, dependent: :destroy
   has_many :assignments, through: :tasks
-  has_many_attached :files
+  has_many_attached :files, dependent: :destroy
 
   validates :name, :description, :due_date, presence: true
 
