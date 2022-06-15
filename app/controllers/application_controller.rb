6af5f34a8cf0d72270b26905616a2fbde19dc0c2
Adriana Osorio <driosorio@gmail.com>
2022-06-15 13:15:58 -0300

    avatar picture working and edit user too

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   # [...]
+  before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
   include Pundit
 
@@ -13,6 +14,14 @@ class ApplicationController < ActionController::Base
     flash[:alert] = "You are not authorized to perform this action."
     @task = Task.find(params[:task_id])
     redirect_to(task_path(@task))
+  end
+
+  def configure_permitted_parameters
+    # For additional fields in app/views/devise/registrations/new.html.erb
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:avatar])
+
+    # For additional in app/views/devise/registrations/edit.html.erb
+    devise_parameter_sanitizer.permit(:account_update, keys: [:avatar])
   end
 
   def after_sign_in_path_for(resource)
