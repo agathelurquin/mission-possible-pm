335dba68b79282025c06b0eafb80edbec4ab87a6
54rtor1 <1929sartori@gmail.com>
2022-06-16 16:54:58 -0300

    back to list button

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,5 +5,7 @@ class PagesController < ApplicationController
   end
 
   def calendar
+    start_date = params.fetch(:created_at, Date.today).to_date
+    @projects = current_user.projects.where(due_date: start_date.beginning_of_month.beginning_of_week..start_date.end_of_month.end_of_week)
   end
 end
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -36,10 +36,6 @@ class ProjectsController < ApplicationController
         project.name.downcase.include?(params[:query].downcase)
       end
     end
-
-    start_date = params.fetch(:created_at, Date.today).to_date
-    @projects = current_user.projects.where(due_date: start_date.beginning_of_month.beginning_of_week..start_date.end_of_month.end_of_week)
-    #@projects = datetime_it(@projects)
   end
 
   def show
