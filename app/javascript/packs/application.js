37c890173227371a28c42595b82ea7a0e7e61248
Agathe Lurquin <agathe.lurquin@essca.eu>
2022-06-17 14:16:56 -0300

    updatess

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -22,6 +22,14 @@ import "@rails/actiontext"
 require("trix")
 require("@rails/actiontext")
 
+var jQuery = require('jquery')
+
+// include jQuery in global and window scope (so you can access it globally)
+// in your web browser, when you type $('.div'), it is actually refering to global.$('.div')
+global.$ = global.jQuery = jQuery;
+window.$ = window.jQuery = jQuery;
+
+
 $(function(){
 
   window.sr = ScrollReveal();
